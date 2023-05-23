88b123bd38cd562a4cf8331f89a0b227d1268415
yunsy1103 <yunsy1103@naver.com>
2023-05-23 23:15:27 +0900

    Initial commit

---
+++ demo112/src/main/java/com/example/demo112/Demo112Application.java
@@ -0,0 +1,13 @@
+package com.example.demo112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo112Application.class, args);
+	}
+
+}
---
+++ demo112/src/test/java/com/example/demo112/Demo112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
